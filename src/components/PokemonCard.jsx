997bd64f2607b9c5e2ec892dac3097306a0b1985
Jocelyn Deloose <deloosejocelyn@gmail.com>
2023-03-28 16:09:26 +0200

    components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,37 @@
-function MyTitle() {
-  return <h1>Hello React</h1>;
-}
-
-export default MyTitle;
-
-
+const card = {
+  width: "400px",
+  padding: "1.5rem",
+  backgroundColor: "lightgreen",
+  borderRadius: "5px",
+  boxShadow: "0px 0px 5px gray",
+  color: "red",
+  fontSize: "1.6rem",
+  textAlign: "center",
+  textShadow: "0px 0px 5px gray",
+};
 
 function PokemonCard() {
-  return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="Bulbizar">
-    <figcaption>Bulbizar</figcaption>
-</figure>
+  const pokemon = pokemonList[1];
+  return (
+    <figure style={card}>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>)
+
 }
 export default PokemonCard;
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
